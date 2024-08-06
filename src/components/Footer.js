90e3e9736f1cf45a2c0bad8c27e8a695edb2cfdf
Bernard Swanepoel <104361159+XoXoTheFrozenFox@users.noreply.github.com>
2024-08-06 13:25:37 +0200

    added files
    added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,7 +17,7 @@ export const Footer = () => {
                 <a href="https://www.linkedin.com/in/bernard-swanepoel-a2777322b/"><img src={navIcon1} alt="" /></a>
                 <a href="https://github.com/XoXoTheFrozenFox"><img src={navIcon2} alt="" /></a>
                 <a href="https://xoxothefrozenfox.github.io/BernardSwanepoelCV/Index.html"><img src={navIcon4} alt="" /></a>
-                <a href="/Bernard_Swanepoel_Resume.pdf" download="Bernard_Swanepoel_Resume.pdf"><img src={navIcon3} alt="" /></a>
+                <a href="/files/Bernard_Swanepoel_Resume.pdf" download="Bernard_Swanepoel_Resume.pdf"><img src={navIcon3} alt="" /></a>
             </div>
             <p>👽👾🚀</p>
           </Col>
